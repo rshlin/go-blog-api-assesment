bfc8323b648de0b4b3bed5d0f8e0607ee026ce4c
rsh <shlinru@gmail.com>
2024-06-23 17:43:49 +0400

    chore: rename child commands

--- cmd/root.go
+++ cmd/root.go
@@ -10,8 +10,8 @@ var rootCmd = &cobra.Command{
 	Use:   "app",
 	Short: "Backend Developer Skill Assessment",
 	Long: `This application contains two commands to complete the Backend Developer Skill Assessment:
-- logicalTest: Takes a string of digits and returns the number of ways it can be decoded back into its original message.
-- technicalTest: Starts a server to handle a RESTful API for a blog platform.`,
+- logical: Takes a string of digits and returns the number of ways it can be decoded back into its original message.
+- technical: Starts a server to handle a RESTful API for a blog platform.`,
 }
 
 func Execute() {
