11356e2c8d499946244a2ef947e99b6b4ce89d50
rsh <shlinru@gmail.com>
2024-06-24 16:44:39 +0400

    fix: add human readable error

--- blog/error/error.go
+++ blog/error/error.go
@@ -6,8 +6,19 @@ type Type int
 
 const (
 	NotFound Type = iota
-	NotAuthorized
+	Forbidden
 )
+
+func (t Type) String() string {
+	switch t {
+	case NotFound:
+		return "Not Found"
+	case Forbidden:
+		return "Forbidden"
+	default:
+		return "Unknown"
+	}
+}
 
 type Error struct {
 	ErrorType Type
@@ -17,9 +28,9 @@ type Error struct {
 
 func (e *Error) Error() string {
 	if e.wrapped != nil {
-		return fmt.Sprintf("ErrorType: %d, message: %s\n%s", e.ErrorType, e.message, e.wrapped.Error())
+		return fmt.Sprintf("ErrorType: %s, message: %s\n%s", e.ErrorType, e.message, e.wrapped.Error())
 	}
-	return fmt.Sprintf("ErrorType: %d, message: %s", e.ErrorType, e.message)
+	return fmt.Sprintf("ErrorType: %s, message: %s", e.ErrorType, e.message)
 }
 
 func NewError(errorType Type, message string, original error) *Error {
